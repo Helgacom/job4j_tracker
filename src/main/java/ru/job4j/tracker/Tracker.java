35cf7faa087d7d5a8fa964a45d4807b957c76c92
Olga Remneva <remneva_olga@mail.ru>
2022-05-26 10:40:51 +0400

    Tracker - хранилище [#396 #304950]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,16 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[items.length];
-        int size = 0;
-        for (int index = 0; index < items.length; index++) {
-            Item el = items[index];
-            if (el != null) {
-                rsl[size] = el;
-                size++;
-            }
-        }
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
