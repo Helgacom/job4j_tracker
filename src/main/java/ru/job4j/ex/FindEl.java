cf6df79b2bf6f623b9e1d822d7c0d8e0866451e0
Olga Remneva <remneva_olga@mail.ru>
2022-06-06 11:44:08 +0400

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else if (i == value.length - 1 && rsl == -1) {
+                throw new ElementNotFoundException("Element not found");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Ivan", "Igor", "Tim"};
+        try {
+            indexOf(names, "Oleg");
+    } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
