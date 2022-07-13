011fc615f07ffc1a4f2db99f1933621f4abb6a2f
Olga Remneva <remneva_olga@mail.ru>
2022-07-13 11:16:24 +0400

    Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        int count = 0;
+        for (String word : origin) {
+            check.add(word);
+        }
+        for (String word : text) {
+            if (check.contains(word)) {
+                count++;
+            }
+        }
+        if (count != check.size()) {
+            rsl = false;
+        }
+        return rsl;
+    }
+}
