235d35e27bc33c3378df776c1a681329c90711a7
Olga Remneva <remneva_olga@mail.ru>
2022-08-01 11:32:03 +0400

    Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/Account.java
+++ src/main/java/ru/job4j/bank/Account.java
@@ -2,7 +2,16 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс описывает модель данных счета
+ *
+ * @author OLGA REMNEVA
+ */
 public class Account {
+    /**
+     * Хранение номера счета осуществляется в поле requisite
+     * Хранение остатка по счету осуществляется в поле balance
+     */
     private String requisite;
     private double balance;
 
@@ -11,22 +20,46 @@ public class Account {
         this.balance = balance;
     }
 
+    /**
+     * Метод позволяет получить номер счета
+     *
+     * @return возвращает номер счета
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Метод позволяет присвоить номер счета при его создании
+     *
+     * @param requisite номер счета и null, если номера нет
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Метод позволяет получить остаток по счету.
+     *
+     * @return остаток по счету или null, если остатка нет
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Метод позволяет назначить остаток по счету
+     *
+     * @param balance остаток по счету
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Метод принимает на вход Object o, проверяет, что он совпадает с текущим объектом
+     * по полю requisite
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +72,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Метод возращает hash - код объекта, сгенерированному по полю requisite
+     *
+     * @return возвращает hash - код объекта, сгенерированному по полю requisite
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу банковского сервиса, который включает:
+ * добавление пользователя, если такого пользователя еще нет в системе;
+ * добавление нового счета к пользователю;
+ * поиск пользователя по номеру паспорта;
+ * поиск счета пользователя по реквизитам;
+ * перевод с одного счета на другой
+ *
+ * @author OLGA REMNEVA
+ * @version 1.0
+ */
 public class BankService {
+    /**
+     * поле содержит всех пользователей системы с привязанными к ним счетами
+     * хранение в справочнике HashMap (ключ - User, значение - счета пользователя в ArrayList)
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Метод добавляет нового пользователя в систему
+     * с использованием метода Map.put(), который принимает объект типа User и
+     * добавляет к нему пустой список для хранения привязанных счетов,
+     * пользователь должен быть уникален, для проверки используем putIfAbsent
+     *
+     * @param user объект типа User, пользователь
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Метод добавляет новый счет к пользователю:
+     * находим пользователя по паспорту findByPassport,
+     * получаем список всех счетов пользователя и добавляем новый счет к ним,
+     * если этого счета еще нет в системе
+     *
+     * @param passport номер паспорта
+     * @param account объект счета
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -22,6 +54,14 @@ public class BankService {
         }
     }
 
+    /**
+     * Метод ищет пользователя по номеру паспорта:
+     * используется перебор всех элементов через for-each
+     * в коллекции, полученной через метод keySet()
+     *
+     * @param passport номер пасспорта
+     * @return возвращает объект User
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
@@ -33,6 +73,15 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод ищет пользователя по реквизитам:
+     * сначала находим пользователя по паспорту, если он найден
+     * получаем список счетов пользователя и находим нужный счет
+     *
+     * @param passport номер паспорта
+     * @param requisite номер счета
+     * @return возвращает объект Account или null, если такого пользователя или счета не существует
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account rsl = null;
         User user = findByPassport(passport);
@@ -48,6 +97,20 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Метод на вход принимает номер паспорта и номер счета для источника и цели.
+     * Метод возвращает true, если счет - источника и цели не равен нулю,
+     * и на счете - источнике достаточно средств для перевода.
+     * Если хотя бы одно из условий не соблюдается, то возвращает false.
+     * При переводе баланс счета источника уменьшается на amount
+     *
+     * @param srcPassport   номер паспорта юзера, со счета которого будет перевод
+     * @param srcRequisite  номер счета, с которого будет перевод
+     * @param destPassport  номер паспорта юзера, на счет которого будет перевод
+     * @param destRequisite номер счета, на который будет перевод
+     * @param amount        сумма перевода
+     * @return возвращает true или false
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
