2d4de0ca884fb1af1cf43851e2b20ec280994a38
Olga Remneva <remneva_olga@mail.ru>
2024-06-19 13:36:30 +0400

    Add HbmTracker

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,132 @@
+package ru.job4j.tracker;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public Item add(Item item) {
+        var session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        var session = sf.openSession();
+        boolean rsl = false;
+        try {
+            session.beginTransaction();
+            int query = session.createQuery(
+                            "UPDATE Item SET name = :fName, created = :Created WHERE id = :fId")
+                    .setParameter("fName", item.getName())
+                    .setParameter("fCreated", item.getCreated())
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            rsl = query > 0;
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public void delete(int id) {
+        var session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+    }
+
+    @Override
+    public List<Item> findAll() {
+        var session = sf.openSession();
+        List<Item> itemList = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            itemList = session.createQuery("FROM Item", Item.class).getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return itemList;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        var session = sf.openSession();
+        List<Item> itemList = new ArrayList<>();
+        try {
+            session.beginTransaction();
+            Query<Item> query = session.createQuery(
+                    "FROM Item as i WHERE i.name = :fKey", Item.class);
+            query.setParameter("fKey", key);
+            itemList = query.getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return itemList;
+    }
+
+    @Override
+    public Item findById(int id) {
+        var session = sf.openSession();
+        Item rsl = null;
+        try {
+            session.beginTransaction();
+            Query<Item> query = session.createQuery(
+                    "FROM Item as i where i.id = :fId", Item.class);
+            query.setParameter("fId", id);
+            rsl = query.uniqueResult();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return rsl;
+    }
+
+    @Override
+    public void close() {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
+
